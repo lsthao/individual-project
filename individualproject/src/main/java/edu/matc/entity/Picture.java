db60e13408f9ec7bb261117410b5c82d38effea3
lsthao <lthao13@madisoncollege.edu>
2018-02-07 19:19:05 -0600

    adding picture entity

--- individualproject/src/main/java/edu/matc/entity/Picture.java
+++ individualproject/src/main/java/edu/matc/entity/Picture.java
@@ -1,4 +1,59 @@
 package edu.matc.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Picture")
 public class Picture {
+
+    @Id
+    @Column(name="PictureID")
+    private int pictureID;
+
+    @Column(name="Picture")
+    private String picture;
+
+    @Column(name="RestaurantID")
+    private int restaurantID;
+
+    @Column(name="Username")
+    private String username;
+
+
+    public int getPictureID() {
+        return pictureID;
+    }
+
+    public void setPictureID(int pictureID) {
+        this.pictureID = pictureID;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public int getRestaurantID() {
+        return restaurantID;
+    }
+
+    public void setRestaurantID(int restaurantID) {
+        this.restaurantID = restaurantID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
 }
