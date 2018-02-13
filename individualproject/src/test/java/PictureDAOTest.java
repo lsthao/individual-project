2b0033021a258e509a1c515dfebcefa2c00dba60
lsthao <lthao13@madisoncollege.edu>
2018-02-12 22:36:30 -0600

    working on tests

--- individualproject/src/test/java/PictureDAOTest.java
+++ individualproject/src/test/java/PictureDAOTest.java
@@ -3,6 +3,9 @@ import edu.matc.persistence.PictureDAO;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PictureDAOTest {
@@ -21,9 +24,46 @@ public class PictureDAOTest {
 
     @Test
     public void addPictureTest() {
-        Picture picture = new Picture("newpicture.jpg", 2, "username");
+        Picture picture = new Picture("newpicturetest.jpg", 2, "username");
         int pictureID = pictureDAO.addPicture(picture);
-        assertEquals(3, pictureID);
+        assertNotEquals(initialNumberOfPictures, pictureDAO.getAllPictures().size());
+        assertEquals("newpicturetest.jpg", picture.getPicture());
+        assertEquals(pictureID, picture.getPictureID());
+    }
+
+    @Test
+    public void getAllPicturesTest() {
+        List<Picture> picturesList = pictureDAO.getAllPictures();
+        assertNotNull(picturesList);
+
+    }
+
+    @Test
+    public void deletePictureTest() {
+        Picture picture = pictureDAO.getPictureByID(1);
+        pictureDAO.delete(picture);
+
+        assertNotEquals(initialNumberOfPictures, pictureDAO.getAllPictures().size());
+        assertNull(pictureDAO.getPictureByID(1));
+
+    }
+
+    @Test
+    public void updatePictureTest() {
+        Picture picture = pictureDAO.getPictureByID(1);
+        picture.setPicture("updated-picture.png");
+
+        pictureDAO.updatePicture(picture);
+
+        assertEquals("updated-picture.png", pictureDAO.getPictureByID(1).getPicture());
+        //TODO: fix updatePicture method so test passes
+
+    }
+
+    @Test
+    public void getByPropertyTest() {
+        List<Picture> pictures = pictureDAO.getPictureByProperty("Username", "username");
+        log.debug(pictures);
 
     }
 
